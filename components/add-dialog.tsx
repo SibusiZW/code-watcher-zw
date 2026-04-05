eef187c9118e707db3eff8bb9a7ff91ec58bc929
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-04-05 20:25:41 +0200

    added add conversation dialog

--- components/add-dialog.tsx
+++ components/add-dialog.tsx
@@ -1,6 +1,9 @@
-import { Plus } from "lucide-react";
+'use client';
+
+import { BadgePlus, Plus } from "lucide-react";
 import { Button } from "./ui/button";
-import { Dialog, DialogTrigger } from "./ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from "./ui/dialog";
+import { Input } from "./ui/input";
 
 export default function AddDialog() {
     return (
@@ -11,6 +14,20 @@ export default function AddDialog() {
                     Create a new conversation
                 </Button>
             </DialogTrigger>
+
+            <DialogContent className="text-center">
+                <DialogTitle>Create new conversation</DialogTitle>
+
+                <form>
+                    <Input placeholder="Enter title of conversation" className="mb-2" required/>
+                    <Button className="w-full bg-blue-600 hover:bg-blue-300" type="submit">
+                        <BadgePlus />
+                        Create converstaion
+                    </Button>
+                </form>
+
+                <DialogDescription>This creates a new  conversation</DialogDescription>
+            </DialogContent>
         </Dialog>
     )
 }
