27c878d9ff8e0bc8cc47730b169705cfbf3f11bd
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-04-05 20:17:05 +0200

    added table edits

--- db/schema.ts
+++ db/schema.ts
@@ -1,8 +1,16 @@
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const conversations = pgTable('conversations', {
-    id: uuid('id').notNull().defaultRandom(),
+    id: uuid('id').primaryKey().notNull().defaultRandom(),
     title: text('title').notNull(),
     userId: text('userId').notNull(),
+    createdOn: timestamp().defaultNow(),
+})
+
+export const messages = pgTable('messages', {
+    id: uuid('id').primaryKey().notNull().defaultRandom(),
+    userPrompt: text().notNull(),
+    response: text().notNull(),
+    conversationId: uuid().references(() => conversations.id),
     createdOn: timestamp().defaultNow(),
 })
