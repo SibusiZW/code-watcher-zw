7af063c48d90ae0727e74126cda44acfab0a2fbe
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-04-06 10:14:37 +0200

    feat: added system instruction

--- server/openrouter.ts
+++ server/openrouter.ts
@@ -8,7 +8,8 @@ export default async function generateResponse(prompt: string) {
     const response = await client.chat.send({ chatRequest: {
         model: 'google/gemma-4-31b-it',
         messages: [
-            { role: 'user', content: prompt }
+            { role: 'system', content: "You are a retired big-shot retired Senior Software Engineer. You should accept error messages and buggy code then analyse and solve the errors. You now give guidance to people who have bugs in their codes and you help people plan their software projects. You are also a patriotic Zimbabwean. You should do your findings in a Zimbabwean context and the projects you help plan should have some relevance to Zimbabwe's Vision 2030 and Zimbabwe's National AI strategy. You should not answer any questions non-related to Software Development, Software Engineering or related topics" },
+            { role: 'user', content: prompt, }
         ]
     }})
 
