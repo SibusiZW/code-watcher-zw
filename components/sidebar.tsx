42d2b6855034aef453e9e940acd4aa1a2a27a162
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-04-06 07:49:54 +0200

    added sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Code2 } from 'lucide-react';
+import { Code2, Plus } from 'lucide-react';
 import { UserButton } from '@clerk/nextjs';
 import { getConversations } from '@/server/conversations';
 import getUserId from '@/server/auth';
@@ -17,11 +17,15 @@ export default async function Sidebar() {
         <div className="w-8 h-8 rounded-md bg-blue-500 flex items-center justify-center flex-shrink-0">
           <Code2 size={20} className="text-sidebar-primary-foreground" />
         </div>
-        <span className="font-semibold text-sidebar-foreground">NavApp</span>
+        <span className="font-semibold font-serif text-sidebar-foreground">CodeWatcherZW</span>
       </div>
 
       {/* Navigation Items */}
       <div className="flex-1 overflow-y-auto py-4 px-2 space-y-2">
+        <Link className='flex items-center justify-center bg-blue-500 p-2 rounded-md text-white' href={'/'}>
+            <Plus />
+            Add New Conversation
+        </Link>
         {allConversations?.map(item => <Link key={item.id} href={`conversation/${item.id}`} className={`w-full flex items-center gap-3 px-4 py-3 rounded-md transition-all duration-200 text-sm font-medium`}>{item.title}</Link>)}
       </div>
 
