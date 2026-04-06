be0f0329cffbfd2a1564c73658fdb02cdc664ad5
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-04-06 10:38:57 +0200

    added eye icon

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Code2, Plus } from 'lucide-react';
+import { Code2, Eye, Plus } from 'lucide-react';
 import { UserButton } from '@clerk/nextjs';
 import { getConversations } from '@/server/conversations';
 import getUserId from '@/server/auth';
@@ -23,8 +23,8 @@ export default async function Sidebar() {
       {/* Navigation Items */}
       <div className="flex-1 overflow-y-auto py-4 px-2 space-y-2">
         <Link className='flex items-center justify-center bg-blue-500 p-2 rounded-md text-white' href={'/'}>
-            <Plus />
-            Add New Conversation
+            <Eye />
+            Manage conversations
         </Link>
         {allConversations?.map(item => <Link key={item.id} href={`conversation/${item.id}`} className={`w-full flex items-center gap-3 px-4 py-3 rounded-md transition-all duration-200 text-sm font-medium`}>{item.title}</Link>)}
       </div>
